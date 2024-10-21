488af790891bf1e787f698eec7fc8c3e9ca51ecc
mickaela-mstr <mickaelam6@gmail.com>
2024-10-21 17:43:22 +0200

    ajout boucle script

--- comptes.sh
+++ comptes.sh
@@ -2,8 +2,17 @@
 
 Annee=$1
 
-nb_locations=$(cat /home/mickaela/Documents/PPE/PPE1-2024moi/TD1/Exercice1/ann/$Annee/*/*.ann | grep -c  'Location')
-echo "Le nombre de Locations est : $nb_locations"
+nb_locations=0
+
+for file in /home/mickaela/Documents/PPE/PPE1-2024moi/TD1/Exercice1/ann/$Annee/*/*.ann
+do
+    count=$(grep -c 'Location' "$file")
+    
+    nb_locations=$((nb_locations + count))
+done
+
+echo "Le nombre de locations est : $nb_locations"
+
 
 
 
